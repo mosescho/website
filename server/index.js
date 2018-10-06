975923f0328a004039d39d80acbaf7b97f7d2844
Moses Cho <perspective201@gmail.com>
2018-10-06 17:09:05 +0900

    set up express server, readying for heroku deployment

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+// middlewares:
+app.use(express.static(`${__dirname}/../public/dist`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/portfolio', (req, res) => {
+  console.log('moses\'s portfolio')
+  res.send('mose\'s portfolio');
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../public/dist/index.html`));
+});
+
+app.listen(PORT, () => {
+  console.log('port is listening at ', 3000);
+});
